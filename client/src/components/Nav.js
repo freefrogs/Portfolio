b93d5ee6aa7221314d15f8b0e45eb84f73f5f896
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-10-19 18:30:16 +0200

    Add Nav style

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Nav.css';
 
 class Nav extends React.Component {
+  onNavClick() {
+    const nav_input = document.querySelector('.nav_input');
+    nav_input.checked = false;
+  }
   render() {
     return (
       <nav>
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" className="nav_input"/>
           <span className="menu">
               <span className="hamburger"></span>
           </span>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/aboutMe">About Me</Link></li>
-            <li><Link to="/portfolio">Portfolio</Link></li>
-            <li><Link to="/animations">Animations</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link className="link" onClick={this.onNavClick} to="/">Home</Link></li>
+            <li><Link className="link" onClick={this.onNavClick} to="/aboutMe">About Me</Link></li>
+            <li><Link className="link" onClick={this.onNavClick} to="/portfolio">Portfolio</Link></li>
+            <li><Link className="link" onClick={this.onNavClick} to="/animations">Animations</Link></li>
+            <li><Link className="link" onClick={this.onNavClick} to="/contact">Contact</Link></li>
           </ul>
         </label>
       </nav>
