84865cfd7c8d1ab53143315147f2501327f6d019
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-03-10 20:25:20 +0100

    Updates

---
+++ precache-manifest.821c748d0ef9d5803ea47aef841570c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb15c984efcf0c099f9efb3f74d5c2e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ffd69abccabc6d03f560",
+    "url": "/Portfolio/static/css/main.7c428ebe.chunk.css"
+  },
+  {
+    "revision": "9519f2c3ac663a2a5072",
+    "url": "/Portfolio/static/js/2.a865c4b6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Portfolio/static/js/2.a865c4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd69abccabc6d03f560",
+    "url": "/Portfolio/static/js/main.6bb65291.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
